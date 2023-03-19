a86bbf33154f8e3b6c90638513c330b81c4b586b
jpozo20 <jefrypozo@gmail.com>
2023-03-19 13:15:48 -0400

    feat: add initial folders slice

---
+++ src/App/store/slices/bookmarkSlice.ts
@@ -0,0 +1,11 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+//function
+
+export const bookmarkSlice = createSlice({
+  name: 'bookmarks',
+  initialState: [],
+  reducers: {},
+})
+
+export default bookmarkSlice.reducer
