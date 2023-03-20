efd55e25ee1fce3a74d6be6b61c321ec9cf7b3d1
jpozo20 <jefrypozo@gmail.com>
2023-03-20 16:12:07 -0400

    feat: configure store and load initial bookmarks

--- src/App/store/slices/bookmarkSlice.ts
+++ src/App/store/slices/bookmarkSlice.ts
@@ -1,11 +1,34 @@
-import { createSlice } from '@reduxjs/toolkit'
+import browser from 'webextension-polyfill';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
-//function
+type BookmarksNodeState = {
+  bookmarks: browser.Bookmarks.BookmarkTreeNode[];
+  error: string | null;
+};
+const initialState: BookmarksNodeState = {
+  bookmarks: [],
+  error: '',
+};
+
+const actionNames = {
+  deleteBookmarks: 'bookmarks/deleteBookmarks',
+} as const;
+
+const deleteBookmarks = createAsyncThunk(
+  actionNames.deleteBookmarks,
+  async (bookmarkIds: string[]) => {
+    if (!bookmarkIds || !bookmarkIds.length) return;
+    bookmarkIds.forEach(
+      async (bookmarkId) => await browser.bookmarks.remove(bookmarkId),
+    );
+  },
+);
 
 export const bookmarkSlice = createSlice({
   name: 'bookmarks',
-  initialState: [],
+  initialState,
   reducers: {},
-})
+});
 
-export default bookmarkSlice.reducer
+export { deleteBookmarks };
+export default bookmarkSlice.reducer;
