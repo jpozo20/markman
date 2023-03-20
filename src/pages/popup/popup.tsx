ef2c41023562f7579470a00685aa9bc708b4c4ed
jpozo20 <jefrypozo@gmail.com>
2023-03-20 13:09:41 -0400

    refactor: move scripts so worker_service works

--- src/pages/popup/popup.tsx
+++ src/pages/popup/popup.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import PopupPage from '../../App/PopupPage';
 
-console.log('popup script');
-
 const node = document.querySelector('#root');
 const root = createRoot(node as Element);
 root.render(<PopupPage />);
