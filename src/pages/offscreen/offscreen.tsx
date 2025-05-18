871ca3f85d9a726c8a402821ce899d4297a2e7ab
jefrypozo <jefrypozo@gmail.com>
2025-05-18 12:03:18 -0400

    feat: Add message handling from and to the extension

--- src/pages/offscreen/offscreen.tsx
+++ src/pages/offscreen/offscreen.tsx
@@ -1,20 +1,33 @@
+import browser from 'webextension-polyfill';
 import { PGliteWorker } from "@electric-sql/pglite/worker";
 
+type OnMessageCallback = browser.Runtime.OnMessageListenerCallback;
+
+
+let worker: Worker;
+let pg: PGliteWorker;
 const log = console.log;
 const error = console.error;
 
 console.log("Worker inside offscreen");
 
-async function loadPg() {
-  const worker = new Worker(
+const handleQuery = (request: any, sendResponse: (response: unknown) => void) => {
+  console.log('sending request to pglite');
+  setTimeout(async () => {
+    const result = await pg.query("select 'Hello world, from PGLite' as message;");
+    sendResponse('this is the response from pglite: ' + result);
+  }, 500);
+}
+
+async function loadPGlite() {
+  worker = new Worker(
     new URL("./pglite-worker.ts", import.meta.url),
     { type: "module" },
   );
 
-  const pg = new PGliteWorker(worker);
-  //const result = await pg.query("select 'Hello world' as message;");
+  pg = new PGliteWorker(worker);
 
-
+  // Messages between the script and PGliteWorker
   worker.onmessage = function ({ data }) {
     switch (data.type) {
       case 'log':
@@ -24,9 +37,26 @@ async function loadPg() {
         log("successfully loaded pglite!");
         break;
       default:
-        log(`ERROR: Unhandled message in worker: ${data.type}`);
+        log(`ERROR: Unhandled message in pglite-worker: ${data.type}`);
     }
   };
+
+  // Messages between the script and the extension
+  const handleMessage: OnMessageCallback = (request: any, sender, sendResponse) => {
+    switch (request.type) {
+      case 'pglite':
+        log('receiving request from: ', sender);
+        handleQuery(request, sendResponse);
+        break;
+      default:
+        console.log('unknown message request');
+        sendResponse('unknown message request');
+        break;
+    }
+    return true;
+  };
+
+  browser.runtime.onMessage.addListener(handleMessage);
 }
 
-loadPg();
+loadPGlite();
